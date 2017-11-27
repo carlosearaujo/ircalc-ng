03d5ee632d89e1eab8ebc253ed7cd000af4d08d2
Carlos E. Araujo <carlos.araujo@mv.com.br>
2017-11-27 09:56:19 -0300

    Change app port

--- src/app/service/operacao.service.ts
+++ src/app/service/operacao.service.ts
@@ -3,11 +3,11 @@ import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map'
-
+  
 @Injectable()
 export class OperacaoService {
 
-  private BASE_URL = 'http://localhost:8080/trade/'
+  private BASE_URL = 'http://localhost:8022/trade/'
 
   constructor(private http: Http) { }
 
